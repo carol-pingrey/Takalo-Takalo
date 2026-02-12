6e4db54728b5f275d460f78a5accfa7bf0167e05
carol-pingrey <tahianachristian.pro@gmail.com>
2026-02-12 10:56:32 +0300

    Update data.sql

--- database/data.sql
+++ database/data.sql
@@ -2,16 +2,30 @@ CREATE DATABASE takalo;
 
 USE takalo;
 
-CREATE TABLE proprietaire (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255),
-    email VARCHAR(255),
-    mdp VARCHAR(255) NOT NULL
+CREATE TABLE utilisateurs (
+    id_utilisateurs INT AUTO_INCREMENT PRIMARY KEY,
+    nom_utilisateurs VARCHAR(255),
+    email_utilisateurs VARCHAR(255),
+    mdp_utilisateurs VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE categories (
+    id_categories INT AUTO_INCREMENT PRIMARY KEY,
+    nom_categories VARCHAR(255),
+    description_categories VARCHAR(255)
 );
 
 CREATE TABLE objets (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_prop INT,
-    nom_obj VARCHAR(255),
+    id_objets INT AUTO_INCREMENT PRIMARY KEY,
+    id_utilisateurs INT,
+    id_categories VARCHAR(255),
     image_obj VARCHAR(255)
+);
+
+CREATE TABLE echanges (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_proproseur INT,
+    obj_proproseur VARCHAR(255),
+    id_receveur INT,
+    obj_receveur VARCHAR(255)
 );
